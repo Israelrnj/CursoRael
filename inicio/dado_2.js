52f3968506b749becea80e0d9ee792614cc53de5
R@3L <israelrodriguesn@hotmail.com>
2023-07-05 23:43:58 -0300

    att 5/7

---
+++ inicio/dado_2.js
@@ -0,0 +1,20 @@
+// sempre definir bons nomes, tantos para os arquivos, identificadores
+
+let preco = 19.90;
+let desconto = 0.4;
+console.log(preco * (1 - desconto));
+                                            // escolha bons nomes para var or let
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "caderno";
+let categoria = "papelaria";
+console.log(nome +" "+ categoria); //espaço entre texto
+
+console.log("Produto: "+ nome  //string (texto) - sequencia de simbolos
+     + ", Categoria: " + categoria
+     + " , preço " + preco 
+     + " , Desconto " + desconto);
+
+console.log(1 +1);
+console.log("1" + 1); //texto
---
+++ inicio/dados_1.js
@@ -0,0 +1,20 @@
+/* sentença de codigos
+* verbo (passos)
+*dados (manipula)
+valor literal (define um numero direto 1,2,3 ou texto)
+*/ 
+
+var nome = "caneta";
+var quantidade = 10;  //var = variavel executar colocando var "especifica oque é = o valor que deseja"
+var preco = 7.9;
+var imposto = 1.9;
+let precoFinal = preco + imposto; //let é a forma mais atual
+
+console.log(nome);
+console.log(quantidade); 
+console.log(imposto);
+console.log(preco);
+console.log (precoFinal);
+
+nome = "caneta azul"; //variavel, na linha 20 alterou o codigo
+console.log (nome);
